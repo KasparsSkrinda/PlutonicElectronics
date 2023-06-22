c576afa9d844b09ce7e0fb513a2720645f04ab17
KasparsSkrinda <136680805+KasparsSkrinda@users.noreply.github.com>
2023-06-22 03:22:56 +0300

    Delete includes directory

--- includes/common.php
+++
@@ -1,5 +0,0 @@
-<?php
-$con = mysqli_connect("fdb1034.awardspace.net", "3931073_kaspars", "Kaspars1738", "3931073_kaspars")or die($mysqli_error($con));
-if(session_status() == PHP_SESSION_NONE){
-    session_start();
-}
